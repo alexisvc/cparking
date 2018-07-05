866259dd6d4412646204d3808b394705b070897b
Jonathan Alexis Valencia - Ceiba Software <jonathan.valencia@ceiba.com.co>
2018-07-05 14:34:32 -0500

    Initial commit. Just has spring boot structure and gradle's files

---
+++ src/main/java/com/dna/cparking/CparkingApplication.java
@@ -0,0 +1,12 @@
+package com.dna.cparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CparkingApplication.class, args);
+	}
+}
